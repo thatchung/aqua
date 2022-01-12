f9274a4d21f81c507a9d73e01034ffdc6c989b9e
khanh <khanh.trieu@riviu.vn>
2022-01-12 11:10:05 +0700

    aa

--- assets/js/main.js
+++ assets/js/main.js
@@ -135,8 +135,59 @@ function initScrollEvent() {
     })
 }
 
+function sendEmail() {
+    Email.send({
+        Host : "smtp.elasticemail.com",
+        Username : "trieuqk@gmail.com",
+        Password : "6EF379414655B83BF26804104E4F82D29B81",
+        To : 'thatchung1@gmail.com',
+        From : "trieuqk@gmail.com",
+        Subject : "Test SendEmail",
+        Body : "And this is the body"
+    }).then(
+      message => alert(message)
+    );
+}
 
+function telephoneCheck(str) {
+  var isphone = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im.test(str);
+  return isphone;
+}
+
+function validateEmail (emailAdress)
+{
+  let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  if (emailAdress.match(regexEmail)) {
+    return true; 
+  } else {
+    return false; 
+  }
+}
 
 $(document).ready(function($) {
-  initScrollEvent(); 
+  initScrollEvent();
+
+  $("#submit_btn").click(function() {
+    let ho = $("#name_form").val();
+    let ten = $("#sub_name_form").val();
+    let sdt = $("#phone_form").val();
+    let email = $("#email_form").val();
+    if(!ho || !ten) {
+        alert('Vui lòng nhập đầy đủ họ tên');
+    } else
+    if(!sdt) {
+        alert('Vui lòng nhập số điện thoại');
+    } else
+    if(!telephoneCheck(sdt)) {
+        alert('Vui lòng nhập đúng định dạng số điện thoại');
+    } else
+    if(!email) {
+        alert('Vui lòng nhập địa chỉ email');
+    } else
+    if(!validateEmail(email)) {
+        alert('Vui lòng nhập đúng địa chỉ email');
+    } else {
+        sendEmail();
+    }
+  });
 })
